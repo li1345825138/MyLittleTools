b8638b8d8d981a70c451903d9cc4ba33e39a17a2
Lengqiang Lin <RK1234559@Gmail.com>
2023-09-13 19:59:59 -0400

    add hold exception function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 /**
  * @author li1345825138
  * @date 2023/7/18
@@ -8,8 +11,21 @@ public class Main {
             CommandProcessor commandProcessor = new CommandProcessor(args[0]);
             commandProcessor.process(args);
         } catch (Exception e) {
+            holdException(e);
             System.err.println(e.getLocalizedMessage());
             CommandProcessor.printHelp();
+        }
+    }
+
+    /**
+     * Hold Exception and print full stack trace to the log file
+     * @param e any exception that will be hold
+     */
+    private static void holdException(Exception e) {
+        try (PrintWriter printWriter = new PrintWriter("error_log.log")) {
+            e.printStackTrace(printWriter);
+        } catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace();
         }
     }
 }
